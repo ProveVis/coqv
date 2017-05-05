57abe1713f963a59d6befce439c2c1d224649289
casliujian <dreammaker2010@yeah.net>
2017-05-05 19:54:19 +0800

    usable xml util

---
+++ utils/test_xml.ml
@@ -0,0 +1,23 @@
+let _ = 
+    Xml_printer.print (Xml_printer.TChannel stdout) (Serialize.of_unit ());
+    print_endline "";
+    Xml_printer.print (Xml_printer.TChannel stdout) (Serialize.of_edit_id 1);
+    print_endline "";
+    Xml_printer.print (Xml_printer.TChannel stdout) (Serialize.of_list (Serialize.of_string) ["1";"2";"3"]);
+    print_endline "";
+    let xparser = Xml_parser.make (Xml_parser.SString "<unit/>") in
+    let xml_unit = Xml_parser.parse xparser in
+    Xml_printer.print (Xml_printer.TChannel stdout) xml_unit;
+    print_endline "";
+    let xparser = Xml_parser.make (Xml_parser.SString "<edit_id val=\"-1\"/>") in
+    let xml_editid = Xml_parser.parse xparser in
+    Xml_printer.print (Xml_printer.TChannel stdout) xml_editid;
+    print_endline "";
+    print_endline (string_of_int (Serialize.to_edit_id xml_editid));
+    let xparser = Xml_parser.make (Xml_parser.SString "<list><string>1</string><string>2</string><string>3</string></list>") in
+    let xml_list = Xml_parser.parse xparser in
+    Xml_printer.print (Xml_printer.TChannel stdout) xml_list;
+    print_endline "";
+    let str_list = Serialize.to_list (Serialize.to_string) xml_list in
+    List.iter (fun s -> print_string (s^" ")) str_list;
+    print_endline ""
