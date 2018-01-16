54fe794156cadb09d267520ee617648878ecc1cd
Jian Liu <terminatorlxj@163.com>
2018-01-16 20:18:23 +0800

    update

--- coqv_8.6/examples/arith.v
+++ coqv_8.6/examples/arith.v
@@ -6,8 +6,45 @@ Fixpoint add n m :=
 Theorem addnS: forall n m, add n (S m) = S (add n m).
 Proof.
   induction n.
+Focus 2.
+Unfocus.
   intros m; simpl.
   reflexivity.
   intros m; simpl.
-  now apply IHn.
+  apply IHn.
+Qed.
+
+(*
+Lemma addn0 : forall n, add n 0 = n.
+Proof.
+ induction n.
+ simpl.
+ reflexivity.
+ simpl.
+ rewrite addnS.
+ rewrite IHn.
+ reflexivity.
+Qed.
+*)
+
+Lemma add_comm : forall n m, add n m = add m n.
+Proof.
+ induction n;
+ intros.
+ simpl.
+ assert (forall x, add x 0 = x).
+ induction x.
+ simpl.
+ reflexivity.
+ simpl.
+ rewrite addnS.
+ rewrite IHx.
+ reflexivity.
+ rewrite H.
+ reflexivity.
+ rewrite addnS.
+ simpl.
+ rewrite addnS.
+ rewrite IHn.
+ reflexivity.
 Qed.
